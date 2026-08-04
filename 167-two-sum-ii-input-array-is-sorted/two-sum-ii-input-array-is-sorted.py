6b3890b8e283e5a1bd4f11a53ceb974471a83a3d
Rohith B <rohithb775@gmail.com>
2026-08-04 18:36:18 +0530

    Time: 4 ms (75.75%) | Memory: 12.8 MB (83.24%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i=0
+        j=len(numbers)-1
+        while(i<j):
+            sum=numbers[i]+numbers[j]
+            if sum==target:
+                return i+1,j+1
+            elif sum>target:
+                j=j-1
+            else:
+                i=i+1
